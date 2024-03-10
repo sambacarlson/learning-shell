f6981f389c40dbac8de4d0243be2d95afa968daa
sambacarlson <sambacarlson@gmail.com>
2024-03-10 16:45:57 +0100

    exercise with variables

--- hello.sh
+++ hello.sh
@@ -12,3 +12,9 @@ currentPath=`pwd`
 
 echo $greetings
 echo "current path == $currentPath "
+
+today=$(date)  # gets today's date on the variable today
+echo date -d "$today" +%A # get's weekday of today
+christmas="2024-12-25"
+christmasDay=$(date -d "$christmas" +%A) # gets weekday of date variable christmas
+echo "christmas this year will be on a ${christmasDay}"
